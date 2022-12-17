0320730c8489b6f74f059e0f276385bd1b2454e6
SirJimKe <jimmymutuku1@gmail.com>
2022-12-17 13:22:58 +0300

    done

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,14 @@
-export function manageFriends(state, action) {
+const initialState = { friends: [] }
+export function manageFriends(state = initialState, action) {
   // your code here
+  switch (action.type){
+    case "friends/add":
+      const newArray = [...state.friends, action.payload]
+      return { friends: newArray};
+    case "friends/remove":
+      const updatedArray = state.friends.filter(friend => friend.id !== action.payload)
+      return { friends: updatedArray};
+    default:
+      return state;
+  }
 }
